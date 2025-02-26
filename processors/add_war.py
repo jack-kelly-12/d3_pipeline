a64308bc27212d3d71889195a17205e4bec01fba
rhit-kellyjc <rhit.kellyjc@gmail.com>
2025-02-26 09:27:36 -0500

    fixing guts for real this time

--- processors/add_war.py
+++ processors/add_war.py
@@ -117,19 +117,6 @@ def calculate_pitching_team_war(pitching_df, park_factors_df, team_clutch):
                                  'FO': 'sum', 'BB': 'sum', 'HB': 'sum', 'SO': 'sum',
                                  'BF': 'sum', 'Pitches': 'sum', 'Season': 'first', 'Division': 'first'}).reset_index()
 
-    def convert_to_baseball_notation(innings):
-        parts = str(innings).split('.')
-        whole_innings = int(parts[0])
-
-        if len(parts) > 1 and parts[1] != '':
-            outs = int(parts[1])
-            decimal_part = outs / 3
-        else:
-            decimal_part = 0
-
-        return whole_innings + decimal_part
-
-    df['IP'] = df['IP'].apply(convert_to_baseball_notation)
     df['ERA'] = (df['ER'] * 9) / df['IP']
     df['IR-A%'] = ((df['Inh Run Score'] / df['Inh Run']) * 100).fillna(0)
     df['RA9'] = (df['R'] / df['IP']) * 9
